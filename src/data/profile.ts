eabaca876570ffdf6107b5f4211570431257133f
Moisés Silva <mofesilva.dev@gmail.com>
2026-04-03 16:07:12 -0300

    feat: update profile data with user info

--- src/data/profile.ts
+++ src/data/profile.ts
@@ -1,6 +1,6 @@
 export const profile = {
     name: "Nerdcave Studio",
     bio: "Streamer • Gamer • Content Creator\nBem-vindo à minha cave!",
-    avatarUrl: "/profile/nerdcave_3.2_perfil_green_bg(2).png",
+    avatarUrl: "/profile/mofesilva.jpeg",
     initials: "NC",
 } as const;
