75c19ccc1ad2d09eda0d36347580c9edadc2a85c
mofesilva <mofesilva.dev@gmail.com>
2026-04-03 17:55:07 -0300

    fix: update profile tags for improved clarity and consistency

--- src/data/profile.ts
+++ src/data/profile.ts
@@ -1,7 +1,7 @@
 export const profile = {
     name: "Nerdcave Studio",
     bio: "Aqui falamos de games de uma forma diferente!",
-    tags: ["Streamer", "Gamer", "Content Creator"],
+    tags: ["Gamer", "Streamer", "Creator", "Pizza Lover"],
     avatarUrl: "/profile/mofesilva.jpeg",
     initials: "NC",
 } as const;
