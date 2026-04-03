562f76f357edfaf00afb2752865aa2b11acbfc15
Moisés Silva <mofesilva.dev@gmail.com>
2026-04-03 15:40:16 -0300

    feat: add footer component

---
+++ src/components/footer.tsx
@@ -0,0 +1,7 @@
+export function Footer() {
+    return (
+        <footer className="py-6 text-center text-xs text-light/30">
+            &copy; {new Date().getFullYear()} Nerdcave
+        </footer>
+    );
+}
