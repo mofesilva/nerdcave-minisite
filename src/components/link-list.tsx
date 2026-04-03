227a33d5f0f707083652100023046ce55bb05cee
mofesilva <mofesilva.dev@gmail.com>
2026-04-03 17:41:09 -0300

    feat: add Nerdcave logo image to LinkList component

--- src/components/link-list.tsx
+++ src/components/link-list.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import Image from "next/image";
 import { links } from "@/data/links";
 import { LinkCard } from "./link-card";
 
@@ -9,6 +10,14 @@ export function LinkList() {
             {links.map((link, i) => (
                 <LinkCard key={link.id} link={link} index={i} />
             ))}
+            <div className="flex justify-center pt-4">
+                <Image
+                    src="/logos/nerdcave_3.2_perfil_green_transparent.png"
+                    alt="Nerdcave"
+                    width={64}
+                    height={64}
+                />
+            </div>
         </div>
     );
 }
